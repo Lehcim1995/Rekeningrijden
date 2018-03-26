2770db58d4a8ea309922ecf2c4e6203e67d35290
Nick Liebregts <n.liebregts@student.fontys.nl>
2018-03-26 10:11:09 +0200

    Invoices added, implementation of JPA not quite done yet.

---
+++ rekeningadministratiemodule/src/main/java/dao/InvoiceDao.java
@@ -0,0 +1,15 @@
+package dao;
+
+import classes.Invoice;
+
+import java.util.List;
+
+public interface InvoiceDao {
+
+    Invoice create(Invoice invoice);
+
+    Invoice update(Invoice invoice);
+
+    List<Invoice> getAllInvoices();
+
+}
---
+++ rekeningadministratiemodule/src/main/java/dao/InvoiceDaoJPA.java
@@ -0,0 +1,48 @@
+package dao;
+
+import classes.Invoice;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Stateless
+public class InvoiceDaoJPA implements InvoiceDao {
+
+    @PersistenceContext(unitName = "accountAdministrationPU")
+    private EntityManager em;
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Invoice create(Invoice invoice) {
+        if (invoice == null) {
+            throw new IllegalArgumentException("Invoice is null");
+        }
+        em.persist(invoice);
+        return invoice;
+    }
+
+    @Override
+    public Invoice update(Invoice invoice) {
+        if (invoice == null) {
+            throw new IllegalArgumentException("Invoice is null");
+        }
+        em.merge(invoice);
+        return invoice;
+    }
+
+    @Override
+    public List<Invoice> getAllInvoices() {
+        try {
+            return em.createQuery("SELECT invoice FROM Invoice invoice", Invoice.class).getResultList();
+        }
+        catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+        return null;
+    }
+}
