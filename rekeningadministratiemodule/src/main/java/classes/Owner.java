10b68842de052a8317a7854d1f728b147b264be9
Nick Liebregts <n.liebregts@student.fontys.nl>
2018-03-26 13:10:48 +0200

    TAT-77-Factuur_maken done

--- rekeningadministratiemodule/src/main/java/classes/Owner.java
+++ rekeningadministratiemodule/src/main/java/classes/Owner.java
@@ -1,4 +1,36 @@
 package classes;
 
-public class Owner {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Owner implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private int citizenID;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String address;
+    private String city;
+
+    public Owner() {}
+
+    public Owner(String firstName, String middleName, String lastName, String address, String city) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+    }
+
+    public Owner(String firstName, String lastName, String address, String city) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+    }
 }
