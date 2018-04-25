ff8e9ff7e81e2978906e1d48ab48a570a03e41e4
Nick Liebregts <n.liebregts@student.fontys.nl>
2018-04-25 10:17:39 +0100

    Road added

---
+++ rekeningadministratiemodule/src/main/java/dao/RoadDao.java
@@ -0,0 +1,19 @@
+package dao;
+
+import classes.Road;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface RoadDao {
+
+    Road createRoad (Road road);
+
+    Road editRoad (Road road);
+
+    boolean deleteRoad (int id) throws SQLException;
+
+    Road findRoadById (int id) throws SQLException;
+
+    List<Road> getAllRoads () throws SQLException;
+}
---
+++ rekeningadministratiemodule/src/main/java/dao/RoadDaoJPA.java
@@ -0,0 +1,63 @@
+package dao;
+
+import classes.Road;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.sql.SQLException;
+import java.util.List;
+
+public class RoadDaoJPA implements RoadDao {
+
+    @PersistenceContext(unitName = "accountAdministrationPU")
+    private EntityManager em;
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Road createRoad(Road road) {
+        if (road == null) {
+            throw new IllegalArgumentException("Road is null");
+        }
+        em.persist(road);
+        return road;
+    }
+
+    @Override
+    public Road editRoad(Road road) {
+        if (road == null) {
+            throw new IllegalArgumentException("Road is null");
+        }
+        em.merge(road);
+        return road;
+    }
+
+    @Override
+    public boolean deleteRoad(int id) throws SQLException {
+        em.remove(findRoadById(id));
+        return true;
+    }
+
+    @Override
+    public Road findRoadById(int id) throws SQLException {
+        try {
+            return em.createQuery("SELECT road FROM Road road WHERE road.id = :id", Road.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+        }
+        catch (Exception e) {
+            throw new SQLException("Could not find road with id: " + id);
+        }
+    }
+
+    @Override
+    public List<Road> getAllRoads() throws SQLException {
+        try {
+            return em.createQuery("SELECT road FROM Road road", Road.class).getResultList();
+        }
+        catch (Exception e) {
+            throw new SQLException("Could not get all roads");
+        }
+    }
+}
