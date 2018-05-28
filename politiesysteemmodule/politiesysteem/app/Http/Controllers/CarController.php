ef63c91093296dacda4d2d24243c0dcd3a7817da
Luukdewaaier <Luuk@scrumble.nl>
2018-05-28 02:37:46 +0200

    removed useless code

--- politiesysteemmodule/politiesysteem/app/Http/Controllers/CarController.php
+++ politiesysteemmodule/politiesysteem/app/Http/Controllers/CarController.php
@@ -23,16 +23,6 @@ class CarController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -52,46 +42,12 @@ class CarController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param Car $car
      * @return \Illuminate\Http\Response
      */
     public function show(Car $car)
     {
         return view('car.show', compact('car'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 
     public function retrieve(Car $car)
