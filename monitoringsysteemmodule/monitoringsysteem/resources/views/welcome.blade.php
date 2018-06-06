ed1f728c90bd8f9b888d6536553bd51366085b51
Luukdewaaier <Luuk@scrumble.nl>
2018-06-06 14:28:01 +0200

    finished front-end

--- monitoringsysteemmodule/monitoringsysteem/resources/views/welcome.blade.php
+++ monitoringsysteemmodule/monitoringsysteem/resources/views/welcome.blade.php
@@ -5,91 +5,71 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Monitoring - Denemarken</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <!-- bootstrap -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
-            .full-height {
-                height: 100vh;
-            }
+        <!-- jQuery import -->
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <!-- Font awesome import -->
+        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+        <div class="container" style="margin: auto auto">
+            <table class="table table-striped table-bordered table-dark table-hover">
+                <thead>
+                    <tr>
+                        <td width="80%"><strong>Domein</strong></td>
+                        <td width="20%"><strong>Status</strong></td>
+                    </tr>
+                </thead>
+                <tbody id="tableBody">
+                    {{-- JavaScript responsible for content --}}
+                </tbody>
+            </table>
         </div>
     </body>
+
+    <script>
+        let endpoints = [
+            ['http://192.168.25.135:8080/', 'home'],
+            ['http://192.168.25.135:8080/verplaatsingsmodule', 'nothome']
+        ];
+
+        $(document).ready(function () {
+            endpoints.forEach(function (endpoint) {
+                $('#tableBody').append('<tr><td>' + endpoint[0] + '</td><td style="text-align: center"><i class="fa fa-spinner fa-spin ' + endpoint[1] + '"></i></td></tr>');
+                checkUptime(endpoint);
+            });
+
+        });
+
+        function checkUptime(url) {
+            let selector = '.' + url[1];
+            $.ajax({
+                url: url[0],
+                type: "GET",
+                success: function (response) {
+                    $(selector).removeClass('fa-spinner');
+                    $(selector).removeClass('fa-spin');
+                    $(selector).removeClass('fa-times');
+                    $(selector).addClass('fa-check');
+                    $(selector).css('color', 'green');
+                },
+                error: function (xhr, status) {
+                    $(selector).removeClass('fa-spinner');
+                    $(selector).removeClass('fa-spin');
+                    $(selector).removeClass('fa-check');
+                    $(selector).addClass('fa-times');
+                    $(selector).css('color', 'red');
+                }
+            });
+        }
+    </script>
 </html>
